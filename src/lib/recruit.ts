3fe2af5abf7299e6982f613994884384cbd88cb1
flakepym <ympark0670@gmail.com>
2026-07-27 21:00:01 +0900

    fixed link

--- src/lib/recruit.ts
+++ src/lib/recruit.ts
@@ -1,2 +1,3 @@
 // Update this when a new recruiting cycle's application form opens.
-export const RECRUIT_FORM_URL = "https://forms.gle/V7NrYFjwvgpkrjUo6";
+export const RECRUIT_FORM_URL =
+  "https://docs.google.com/forms/d/e/1FAIpQLScQQDBR86VQTi8d9QNIYS-YxswumVENwogRsbzVuIxmIs90ZQ/viewform";
